8f7cf7d4fca113f6199ee1dd85d0f20056870ce6
atc73 <joris.sellam@gmail.com>
2020-06-01 23:11:04 +0200

    third

--- public/app.js
+++ public/app.js
@@ -1,10 +1,13 @@
 "use strict";
 // classes
 var Invoice = /** @class */ (function () {
-    function Invoice(c, d, a) {
-        this.client = c;
-        this.details = d;
-        this.amount = a;
+    // readonly client: string;
+    // public details: string;
+    // private amount: number;
+    function Invoice(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
     }
     Invoice.prototype.format = function () {
         return this.client + " owes " + this.amount + " for " + this.details;
--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
 // classes
 class Invoice {
-  readonly client: string;
-  public details: string;
-  private amount: number;
+  // readonly client: string;
+  // public details: string;
+  // private amount: number;
 
-  constructor(c: string, d: string, a: number) {
-    this.client = c;
-    this.details = d;
-    this.amount = a;
-  }
+  constructor(
+    readonly client: string,
+    private details: string,
+    public amount: number
+  ) {}
 
   format() {
     return `${this.client} owes ${this.amount} for ${this.details}`;
